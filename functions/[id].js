73fdf15fecdc317ebcf73ead32d9ed925345dabf
joedevz <ujanganusholeh@gmail.com>
2026-09-07 02:40:33 +0700

    Update [id].js

--- functions/[id].js
+++ functions/[id].js
@@ -2,14 +2,18 @@ export async function onRequestGet(context) {
   const { params, env, request } = context;
   const id = params.id;
 
+  // 1. Ambil data dari KV
   const longUrl = await env.URL_DB.get(id);
 
   if (longUrl) {
-
+    // 2. Jika ID ditemukan, redirect ke link asli
     return Response.redirect(longUrl, 302);
   }
 
+  // 3. JIKA ID TIDAK DITEMUKAN:
+  // Ambil URL dasar (origin) contoh: https://viedey.pages.dev
   const url = new URL(request.url);
   
+  // Arahkan (Redirect) ke halaman 404.html yang kita buat tadi
   return Response.redirect(`${url.origin}/404.html`, 302);
 }
